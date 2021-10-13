45b820d2ed88443e8b7305d50e74e31c12380583
Jan Kenette Dagal <kenettedagal@gmail.com>
2021-10-13 10:25:24 +0800

    Add display function

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+
+let displayValue = "";
+const output = document.querySelector('.output');
+const btn0 = document.querySelector('.n0');
+const btn1 = document.querySelector('.n1');
+const btn2 = document.querySelector('.n2');
+const btn3 = document.querySelector('.n3');
+const btn4 = document.querySelector('.n4');
+const btn5 = document.querySelector('.n5');
+const btn6 = document.querySelector('.n6');
+const btn7 = document.querySelector('.n7');
+const btn8 = document.querySelector('.n8');
+const btn9 = document.querySelector('.n9');
+
 function add(a, b){
-    return a + b;
+    return (a + b);
 }
 
 function subtract(a, b){
@@ -19,55 +33,63 @@ function operate(a, operator, b){
     
 }
 
-const btn0 = document.querySelector('.n0');
-const btn1 = document.querySelector('.n1');
-const btn2 = document.querySelector('.n2');
-const btn3 = document.querySelector('.n3');
-const btn4 = document.querySelector('.n4');
-const btn5 = document.querySelector('.n5');
-const btn6 = document.querySelector('.n6');
-const btn7 = document.querySelector('.n7');
-const btn8 = document.querySelector('.n8');
-const btn9 = document.querySelector('.n9');
+function setDisplay(){
+    output.textContent = (displayValue);
+}
+
+
 
 btn0.addEventListener('click', function(){
-    console.log('btn0 clicked');
+   displayValue += "0";
+   setDisplay();
+
 });
 
 btn1.addEventListener('click', function(){
-    console.log('btn1 clicked');
+    displayValue += "1";
+    setDisplay();
+
 });
 
 btn2.addEventListener('click', function(){
-    console.log('btn2 clicked');
+    displayValue += "2";
+    setDisplay();
+
 });
 
 btn3.addEventListener('click', function(){
-    console.log('btn3 clicked');
+    displayValue += "3";
+   setDisplay();
 });
 
 btn4.addEventListener('click', function(){
-    console.log('btn4 clicked');
+    displayValue += "4";
+   setDisplay();
 });
 
 btn5.addEventListener('click', function(){
-    console.log('btn5 clicked');
+    displayValue += "5";
+   setDisplay();
 });
 
 btn6.addEventListener('click', function(){
-    console.log('btn06 clicked');
+    displayValue += "6";
+   setDisplay();
 });
 
 btn7.addEventListener('click', function(){
-    console.log('btn7 clicked');
+    displayValue += "7";
+   setDisplay();
 });
 
 btn8.addEventListener('click', function(){
-    console.log('btn8 clicked');
+    displayValue += "8";
+   setDisplay();
 });
 
 btn9.addEventListener('click', function(){
-    console.log('btn9 clicked');
+    displayValue += "9";
+   setDisplay();
 });
 
 
