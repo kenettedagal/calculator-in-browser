5d4b966c826285bc1134b040d7b2fcd8a96934d8
Jan Kenette Dagal <kenettedagal@gmail.com>
2021-10-15 14:50:31 +0800

    Fix equal and addition button functionality

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 
 let displayValue = "";
+let firstValue = "";
+let secondValue = "";
+let operator = "";
+let equalBtnPressed = false;
 const output = document.querySelector('.output');
 const btn0 = document.querySelector('.n0');
 const btn1 = document.querySelector('.n1');
@@ -11,9 +15,16 @@ const btn6 = document.querySelector('.n6');
 const btn7 = document.querySelector('.n7');
 const btn8 = document.querySelector('.n8');
 const btn9 = document.querySelector('.n9');
+const addBtn = document.querySelector('.add');
+const subtractBtn = document.querySelector('.subtract');
+const multiplyBtn = document.querySelector('.multiply');
+const divideBtn = document.querySelector('.divide');
+const equalBtn = document.querySelector('.equals');
+
+
 
 function add(a, b){
-    return (a + b);
+    return (Number(a) + Number(b));
 }
 
 function subtract(a, b){
@@ -29,67 +40,190 @@ function divide(a, b){
 }
 
 function operate(a, operator, b){
-
+    switch (operator) {
+        case "add":
+            displayValue = add(Number(a), Number(b));
+            setDisplayValue();
+            // storeFirstValue();
+            // clearSecondValue();
+            
+    }    
     
 }
-
-function setDisplay(){
+let operatorCount = 0;
+//user inputs
+    //store value 1
+    //press operator
+    //store value 2 after pressing = or any operator
+    // call operate
+    //display the value
+function storeFirstValue(){
+    firstValue = displayValue;
+}
+function storeSecondValue(){
+    secondValue = displayValue;
+}
+function clearFirstValue(){
+    firstValue = "";
+}
+function clearSecondValue(){
+    secondValue = "";
+}
+function clearOperator(){
+    operator = "";
+}
+function incrementOperatorCount(){
+    operatorCount++;
+}
+function resetOperatorCount(){
+    operatorCount = 0;
+}
+function clearDisplayValue(){
+    displayValue = "";
+    setDisplayValue();
+}
+function setDisplayValue(){
     output.textContent = (displayValue);
 }
+// input num1 
+// press +
+//store num1 into var a
+//clear display
+//input num 2
+//press + 
+// add var a + b
+//show sum
+//store sum in var a
+//if press digit
+//remove the display
+// show what pressed
 
 
+// OPERATORS
+addBtn.addEventListener('click', function(){
+    operator += "add"
+    storeFirstValue();
+    clearDisplayValue();
 
+    
+});
+subtractBtn.addEventListener('click', function(){
+    operator += "sub";
+    
+    
+});
+multiplyBtn.addEventListener('click', function(){
+    operator += "mul";
+    
+    
+});
+divideBtn.addEventListener('click', function(){
+    operator += "div";
+    
+    
+});
+equalBtn.addEventListener('click', function(){
+    storeSecondValue();
+    operate(firstValue, operator, secondValue);
+    clearFirstValue();
+    clearSecondValue();
+    equalBtnPressed = true;
+    clearOperator();
+    
+});
+
+// DIGITS
 btn0.addEventListener('click', function(){
-   displayValue += "0";
-   setDisplay();
+   if(equalBtnPressed) {
+       equalBtnPressed = false;
+       clearDisplayValue();
+   }
+    displayValue += "0";
+   setDisplayValue();
 
 });
 
 btn1.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "1";
-    setDisplay();
+    setDisplayValue();
 
 });
 
 btn2.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "2";
-    setDisplay();
+    setDisplayValue();
 
 });
 
 btn3.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "3";
-   setDisplay();
+   setDisplayValue();
 });
 
 btn4.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "4";
-   setDisplay();
+   setDisplayValue();
 });
 
 btn5.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "5";
-   setDisplay();
+   setDisplayValue();
 });
 
 btn6.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "6";
-   setDisplay();
+   setDisplayValue();
 });
 
 btn7.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "7";
-   setDisplay();
+   setDisplayValue();
 });
 
 btn8.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "8";
-   setDisplay();
+   setDisplayValue();
 });
 
 btn9.addEventListener('click', function(){
+    if(equalBtnPressed) {
+        equalBtnPressed = false;
+        clearDisplayValue();
+    }
     displayValue += "9";
-   setDisplay();
+   setDisplayValue();
 });
 
 
